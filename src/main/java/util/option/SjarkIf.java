cc46513d048a7c033ed0a48edd8494970f2958e4
MasterKun94 <jsczcmk@outlook.com>
2019-05-16 22:56:32 +0800

    SJARK!!!

--- src/main/java/util/option/SjarkIf.java
+++ src/main/java/util/option/SjarkIf.java
@@ -1,13 +1,22 @@
 package util.option;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class SjarkIf<E> {
 
     public SjarkIf(Predicate<? super E> predicate, Supplier<E> sjarkSupplier) {
-
+        this.predicate = predicate;
     }
+
+    private Predicate<? super E> predicate;
+
+    private SjarkPip<E, ?> thisPip;
+
+    private SjarkPip<E, ?> thatPip;
+
 
     public Sjark<E> then() {
         return null;
@@ -15,5 +24,18 @@ public class SjarkIf<E> {
 
     public Sjark<E> orElse() {
         return null;
+    }
+
+    private Consumer<E> thisConsumer;
+
+    private Consumer<E> thatConsumer;
+
+    private Supplier<E> supplier;
+
+    public Consumer<Supplier<E>> getConsumer() {
+        return supplier -> {
+            E e = supplier.get();
+            (predicate.test(e) ? thisPip.getConsumer() : thatPip.getConsumer()).accept(supplier);//TODO
+        };
     }
 }
